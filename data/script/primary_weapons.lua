b65dd20c951bff8129d89c60c2f3bc26ad062694
Calle Laakkonen <laakkonenc@gmail.com>
2026-01-10 14:19:50 +0200

    Change delta bomber primary weapon firing pattern
    A center shot is vital as without it, the player cannot get the ship
    unstuck if stuck in terrain.

--- data/script/primary_weapons.lua
+++ data/script/primary_weapons.lua
@@ -19,9 +19,7 @@ function weapons.cannon(ship)
 end
 
 function weapons.delta_cannon(ship)
-	-- Dual cannons means (almost) twice as fast firing rate
-	-- The offset positions make digging harder as a tradeoff
-	ship.primary_weapon_cooldown = 0.09
+	ship.primary_weapon_cooldown = 0.10
 
 	local bullet = {
 		vel = ship.vel + Vec2_for_angle(-ship.angle, 1000.0),
@@ -34,13 +32,17 @@ function weapons.delta_cannon(ship)
 		},
 	}
 
-	if ship.state.barrel_switch then
+	if ship.state.barrel_switch == 1 then
 		bullet.pos = ship.pos + Vec2_for_angle(-ship.angle + 60, 16)
-	else
+		ship.state.barrel_switch = 2
+	elseif ship.state.barrel_switch == 2 then
 		bullet.pos = ship.pos + Vec2_for_angle(-ship.angle - 60, 16)
+		ship.state.barrel_switch = 0
+	else
+		bullet.pos = ship.pos
+		ship.state.barrel_switch = 1
 	end
 	game.effect("AddBullet", bullet)
-	ship.state.barrel_switch = not ship.state.barrel_switch
 end
 
 return weapons
